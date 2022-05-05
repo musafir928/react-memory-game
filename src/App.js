5b8b7b0e82793743f58562784c7f063f8c26b82a
musafir928 <uganoghli@gmail.com>
2022-05-05 14:02:57 +0200

    add card grid display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from 'react/cjs/react.production.min'
+import { useState } from 'react'
 import './App.css'
 
 const cardImages = [
-  {"src": "/img/helmet-1.img"},
-  {"src": "/img/potion-1.img"},
-  {"src": "/img/ring-1.img"},
-  {"src": "/img/scroll-1.img"},
-  {"src": "/img/shield-1.img"},
-  {"src": "/img/sword-1.img"}
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"}
 ]
 
 function App() {
@@ -21,15 +21,26 @@ const [turns, setTurns] = useState(0)
     .map((card)=>({...card, id: Math.random()}))
 
     setCards(shuffledCards)
-    setCards(0)
+    setTurns(0)
   }
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card=> (
+          <div className="card" key={card.id}>
+            <div className="">
+              <img src={card.src} alt="card-front" className="front" />
+              <img src="/img/cover.png" alt="card-back" className="back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App
