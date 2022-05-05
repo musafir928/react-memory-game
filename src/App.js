3bc1cc4325f4f76789dac99964e87e816fbcfbb7
musafir928 <uganoghli@gmail.com>
2022-05-05 16:16:22 +0200

    add compare function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,83 @@
-import { useState } from 'react'
-import './App.css'
-import SingleCard from './components/SingleCard'
+import { useState } from "react"
+import { useEffect } from "react/cjs/react.production.min"
+import "./App.css"
+import SingleCard from "./components/SingleCard"
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
+    { src: "/img/helmet-1.png" , matched: false},
+    { src: "/img/potion-1.png", matched: false },
+    { src: "/img/ring-1.png" , matched: false},
+    { src: "/img/scroll-1.png" , matched: false},
+    { src: "/img/shield-1.png" , matched: false},
+    { src: "/img/sword-1.png", matched: false },
 ]
 
 function App() {
-const [cards, setCards] = useState([])
-const [turns, setTurns] = useState(0)
-  
-  // shuffle cards
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-    .sort(()=>Math.random()-0.5)
-    .map((card)=>({...card, id: Math.random()}))
+    const [cards, setCards] = useState([])
+    const [turns, setTurns] = useState(0)
+    const [choiceOne, setChoiceOne] = useState(null)
+    const [choiceTwo, setChoiceTwo] = useState(null)
 
-    setCards(shuffledCards)
-    setTurns(0)
-  }
+    // shuffle cards
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }))
 
-  return (
-    <div className="App">
-      <h1>Magic Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
+        setCards(shuffledCards)
+        setTurns(0)
+    }
 
-      <div className="card-grid">
-        {cards.map(card=> (
-          <SingleCard key={card.id} card={card}/>
-        ))}
-      </div>
-    </div>
-  )
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
+
+    // compare two turned cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+            if (choiceOne.src === choiceTwo.src) {
+				setCards(prevCards=> {
+					return prevCards.map(card=> {
+						if(card.src === choiceOne.src) {
+							return {...card, matched: true}
+						} else {
+							return card
+						}
+					})
+				})
+				// check if all cards are 
+				if (!cards.filter(card=>!card.matched).length) {
+					console.log(" game over")
+				}
+                resetTurn()
+            } else {
+                resetTurn()
+            }
+        }
+    }, [choiceOne, choiceTwo])
+
+    const resetTurn = () => {
+        setChoiceOne(null)
+        setChoiceTwo(null)
+        setTurns((p) => p + 1)
+    }
+
+    return (
+        <div className='App'>
+            <h1>Magic Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+
+            <div className='card-grid'>
+                {cards.map((card) => (
+                    <SingleCard
+                        key={card.id}
+                        card={card}
+                        handleClick={handleChoice}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,15 @@
-import React from "react"
 import './SingleCard.css'
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleClick }) => {
     return (
         <div className='card'>
             <div>
                 <img src={card.src} alt='card-front' className='front' />
-                <img src='/img/cover.png' alt='card-back' className='back' />
+                <img 
+                src='/img/cover.png' 
+                alt='card-back' 
+                className='back' 
+                onClick={()=>handleClick(card)}/>
             </div>
         </div>
     )
